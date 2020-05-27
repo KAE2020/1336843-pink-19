a226b75a1fbeed61dc1ab0607e9f99febdf01fc0
KAE2020 <60177053+KAE2020@users.noreply.github.com>
2020-05-27 11:38:15 +0800

    Добавляет сборку 2

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task("style", function () {
       autoprefixer()
     ]))
     .pipe(csso())
-    .pipe(rename("style.min.css"))
+    .pipe(rename("style.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
